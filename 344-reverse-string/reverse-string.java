aa9c70badd01ab646cd667a89d0fefddba17c084
Rohit Dongare <rohitdongare581@gmail.com>
2026-09-04 17:21:42 +0530

    Time: 0 ms (100.00%) | Memory: 48.2 MB (73.98%) - LeetSync

--- 344-reverse-string/reverse-string.java
+++ 344-reverse-string/reverse-string.java
@@ -3,9 +3,9 @@ class Solution {
         int start = 0;
         int end = arr.length-1;
         while(start < end){
-            char ch = arr[start];
+            char temp = arr[start];
             arr[start] = arr[end];
-            arr[end] = ch;
+            arr[end] = temp;
             start++;
             end--;
         }
