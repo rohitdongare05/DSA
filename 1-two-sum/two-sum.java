f5c1cf80d4b079ce4ee5a6f56494c48b1ced9f81
Rohit Dongare <rohitdongare581@gmail.com>
2026-09-25 15:29:40 +0530

    Time: 35 ms (44.06%) | Memory: 47 MB (74.08%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,6 +1,7 @@
 class Solution {
     public int[] twoSum(int[] arr, int target) {
         
+        //BRUTE FORCE
         int ans[] = new int[2];
         for(int i=0; i<arr.length; i++){
             for(int j=i+1; j<arr.length; j++){
@@ -12,6 +13,11 @@ class Solution {
             }
         }
         return ans;
+
+
+
+
+        //OPTIMIZED CODE!
         // HashMap<Integer, Integer> map = new HashMap<>();
 
         // for(int i=0;i<nums.length;i++){
